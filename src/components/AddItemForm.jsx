275dfe76b42b7d25e9e5ae67d1a4acabe3f9a4d3
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-02-18 11:39:37 -0800

    Item form completed

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,10 +1,36 @@
+import { useRef, useState } from "react";
 import Button from "./Button";
 
-export default function AddItemForm() {
+export default function AddItemForm({ onAddItem }) {
+  const [itemText, setItemText] = useState("");
+  const inputRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // basic validation
+    if (!itemText) {
+      alert("Please enter an item name");
+      inputRef.current.focus();
+      return;
+    }
+
+    onAddItem(itemText);
+    setItemText("");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
-      <input type="text" />
+      <input
+        ref={inputRef}
+        type="text"
+        value={itemText}
+        onChange={(event) => {
+          setItemText(event.target.value);
+        }}
+        autoFocus
+      />
       <Button label="Add to list" />
     </form>
   );
